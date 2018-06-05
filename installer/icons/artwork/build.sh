d803e0f4c21cf68b2d1948ec67beeb4bc9304864
Nick Piacentine <npiace@mars.asu.edu>
2018-06-04 21:24:00 -0700

    Removing files that aren't needed

--- installer/icons/artwork/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-LOGO='artwork_4096x4096.png'
-
-for SIZE in 71x71 150x150 300x300 1440x2160 2160x2160; do
-	convert -verbose $LOGO -resize $SIZE -gravity center -background transparent -extent $SIZE "artwork_$SIZE.png" 
-done
--- source/net/filebot/subtitle/SubtitleDecoder.java
+++
@@ -1,9 +0,0 @@
-package net.filebot.subtitle;
-
-import java.util.stream.Stream;
-
-public interface SubtitleDecoder {
-
-	Stream<SubtitleElement> decode(String file);
-
-}
